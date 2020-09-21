78d7001bd906479b6e882b8128dd8ff8811ca174
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-09-21 19:00:33 -0400

    Initial Yo

---
+++ src/webparts/roadmap/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IRoadmapWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'RoadmapWebPartStrings' {
+  const strings: IRoadmapWebPartStrings;
+  export = strings;
+}
